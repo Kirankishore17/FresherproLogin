4a4ec7ac1d30a6d37511ce973a23db818d626da8
Kirankishore <39197993+Kirankishore17@users.noreply.github.com>
2020-04-04 21:31:32 +0530

    Initial commit

---
+++ src/main/java/com/kk/FresherproLogin/FresherproLoginApplication.java
@@ -0,0 +1,13 @@
+package com.kk.FresherproLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FresherproLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FresherproLoginApplication.class, args);
+	}
+
+}
